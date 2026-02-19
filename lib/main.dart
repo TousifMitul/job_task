735babb62323d6246c50355112b2ca790dfa014f
TousifMitul <tousif5robbani@gmail.com>
2026-02-19 23:01:50 +0600

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:job_task/features/home/home_controller.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 
@@ -18,4 +17,5 @@ void main () async{
 
  await androidImplementation?.requestNotificationsPermission();
   runApp( MyApp());
+
 }
